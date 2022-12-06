aaf846ffd46fe42d91f3b37de97877aa71fe0d06
Mina Vu <mina8@ada.cs.pdx.edu>
2022-12-06 12:21:38 -0800

    Added AddOns ABC and updated Attacks and Items

--- attacks.h
+++ attacks.h
@@ -22,7 +22,20 @@ Items class is derived from Attacks class simply for dynamic binding in AddOnsDb
 
 using namespace std;
 
-class Attacks
+class AddOns {
+	protected:
+		AddOns* next;
+		AddOns* prev;
+
+	public:
+		AddOns();
+		virtual void display(ostream &) const = 0;
+		virtual int use() = 0;
+		AddOns* & nextLink();	// get next pointer
+		AddOns* & prevLink();	// get prev pointer
+};
+
+class Attacks : public AddOns
 {
 private:
 	string move;
@@ -32,17 +45,9 @@ private:
 	int pp;
 	int maxPP;
 
-protected:
-	Attacks *next;
-	Attacks *prev;
-
 public:
-	Attacks();												// default constructor
-	Attacks(const char *, const char *, int, int, int = 5); // arg constructor
-	Attacks(string, string, int, int, int);					// arg constructor
-	virtual ~Attacks();										// destructor
-
-	virtual void display(ostream &) const;					// display data members
+	void display(ostream &) const;					// display data members
+	int use();										// decrement pp by one to use, return false if pp is 0 for attack
 	bool compareType(const Attacks &) const;				// compare the type with another object's type
 	bool compareType(string) const;							// compare the type with input
 	int calcDamage(const float *, const int) const;			// calculate damage
@@ -50,30 +55,19 @@ public:
 	string typ() const;										// return type for damage calculation
 	int pow() const;										// return power for damage calculation
 	int acc() const;										// return accuracy for damage calculation
-	bool useAttack();										// decrement pp by one to use, return false if pp is 0 for attack
 	bool restore();											// restore pp to maxPP
-	virtual int useItem() const;							// only for using dynamic binding for function in Items
 	friend ostream &operator<<(ostream &, const Attacks &); // overload << operator
-
-	Attacks *&nextLink(); // get next pointer
-	Attacks *&prevLink(); // get prev pointer
 };
 
-class Items : public Attacks
+class Items : public AddOns
 {
 private:
 	string name;
 	int hp;
 
 public:
-	Items();				  // default constructor
-	Items(const char *, int); // arg constructor
-	Items(string, int);		  // arg constructor
-	Items(const Items &);	  // copy constructor
-	~Items();				  // destructor
-
 	void display(ostream &) const; // display data members
-	int useItem() const;		   // return hp of item for usage
+	int use();		   // return hp of item for usage
 };
 
 #endif
